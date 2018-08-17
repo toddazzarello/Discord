1142db4f7d95394df90dcc48a1b0b6c796456925
Todd Azzarello <todd.azzarello@gmail.com>
2018-08-17 16:09:07 -0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,36 @@
+import Discord
+from discord.ext import commands
+
+prefix = ">>>"
+bot = commands.Bot(command_prefix=prefix)
+TOKEN = XXXXXXXXXXXXXXXXXXXXXXX
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+@bot.event
+async def on_message(message):
+    print("The message's content was", message.content)
+
+
+@bot.command()
+async def ping(ctx):
+    '''
+    This text will be shown in the help command
+    '''
+
+    # Get the latency of the bot
+    latency = bot.latency  # Included in the Discord.py library
+    # Send it to the user
+    await ctx.send(latency)
+    
+@bot.command
+async def purge(number)
+    
+
+bot.run(TOKEN)
